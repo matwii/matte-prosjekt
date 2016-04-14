777fe954e91f7423773a465abc0fe59495741802
Hans Brenna <hans_brenna@hotmail.com>
2016-04-14 15:55:38 +0200

    ehh

--- oppgave5.m
+++ oppgave5.m
@@ -5,27 +5,33 @@ disp(['       n            ', 'tilnm�rmet             ', 'feil'])
 disp(['-----------------------------------------'])
 
 format long;
+num_y = ones(11,1);
+error = ones(11,1);
+ 
 
+
+    E=1.3*10^10; %Youngs modulus
+    L=2; %Lengde
+    d = 0.03; %Diameter
+    w = 0.3; %Bredde
+    g=-9.81; %Gravitasjon
+    D=480; %Tetthet
+%     h=L/n; %H�yde
+    f=D*d*g*w; %Funksjon
+    I=w*d^3/12;
+    x = 2;
+    a=(f/(24*E*I))*(x^2)*((x^2)-(4*L*x)+(6*(L^2)));    
 for k = 1:11
     n=10*(2^k);
     y=lagY(n);
     
     
-E=1.3*10^10; %Youngs modulus
-L=2; %Lengde
-d = 0.03; %Diameter
-w = 0.3; %Bredde
-g=-9.81; %Gravitasjon
-D=480; %Tetthet
-h=L/n; %H�yde
-f=D*d*g*w; %Funksjon
-I=w*d^3/12;
-x = 2;    
     
     
+    num_y(k,1) = y(n);
+    error(k,1) = abs(y(n)-a);
     
-    a=(f/(24*E*I))*(x^2)*((x^2)-(4*L*x)+(6*(L^2)));
-    disp([n  y(n) abs(y(n)-a)]);
 end
+ T = table(num_y,error)
 end
 
